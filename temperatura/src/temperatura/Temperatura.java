cfc25f1d3e74f5c2ef2921e12b5af99f98316030
aitor <aitor@aitor-laptop>
2021-12-02 17:18:03 +0100

    Programa que muestra un mensaje de bienvenida.

---
+++ temperatura/src/temperatura/Temperatura.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package temperatura;
+
+/**
+ *
+ * @author aitor
+ */
+public class Temperatura {
+
+    public static void main(String[] args) {
+        System.out.println("Bienvenida");
+    }
+    
+}
